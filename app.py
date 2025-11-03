79d7a9761da3f5d659c6eb415d62c86b9a545529
ShivamMantri23 <shivammantri23@gmail.com>
2025-11-03 15:18:58 +0530

    Merged

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    
+    details = {
+        'Name': 'Shivam Mantri',
+        'Institution': 'Centre of Development of Advanced Technology (CDAC), Noida',
+        'Course': 'Post Graduate-Diploma in Artificial Intelligence (PG-DAI)',
+        'skills': 'Python, Java, C/C++, Linux, MERN, Machine Learning, RHEL',
+        'location': 'Jaipur, Rajasthan',
+        'time': datetime.now().strftime("%A, %d %B %Y, %I:%M %p")
+    }
+    return render_template('index.html', details=details)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
